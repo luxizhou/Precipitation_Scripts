adf1a6d165c2fedf41b0c39c808b36c8903c7c96
lzhou <luxi.zhou@outlook.com>
2021-09-16 10:00:55 +0800

    in middle of era5 data trouble shooting

--- Extract_ERA5_Precipitation.py
+++ Extract_ERA5_Precipitation.py
@@ -10,7 +10,6 @@ import os
 import numpy as np
 import pandas as pd
 import xarray as xr
-import cartopy.crs as ccrs
 from datetime import datetime, timedelta
 from shapely.geometry import LineString, Point, Polygon, MultiPoint
 import matplotlib.pyplot as plt
@@ -25,7 +24,7 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 shapely.speedups.enabled
 
 #%% set parameters
-zone = 1000000.      # buffer zone around tracks, to be consistent with input nodes/track file
+zone = 1500000.      # buffer zone around tracks, to be consistent with input nodes/track file
 pre_days = 1        # number of days before the node's time to include precipitation   
 post_days = 2       # number of days after hte node's time to include precipitation
 
@@ -33,7 +32,7 @@ proj_epsg = 2345    # projeced CRS
 init_epsg = 4326    # geographical CRS
 #%%
 
-ERA5_folder = '/media/lzhou/Extreme SSD/Precipitation/ERA5'
+ERA5_folder = '/home/lzhou/Precipitation/Data/ERA5'
 CMA_folder = '/home/lzhou/Precipitation/TC_Tracks/CMA_Historical_Data/Wind_Rainfall/'
 Output_folder = '/home/lzhou/Precipitation/Output'
 Output_folder2 = '/home/lzhou/Precipitation/Precipitation_Scripts/Output/'
@@ -47,12 +46,13 @@ cn_shape.reset_index(drop=True,inplace=True)
 cma_track_file = 'CMA_Tracks_Nodes_'+str(int(zone/1000))+'km.shp'
 nodes = gpd.read_file(os.path.join(Output_folder2,cma_track_file))
 nodes['Time'] = pd.to_datetime(nodes[['Year','Month','Day']]) 
+nodes_proj = nodes.to_crs(proj_epsg)
 
 #%% read in era5 data
 files = os.listdir(ERA5_folder)
 era5_files = [x for x in files if 'nc' in x]
 era5_files.sort()
-for era5_file in era5_files[:35]:
+for era5_file in era5_files[1:2]:
     print(era5_file)
     era5_file = os.path.join(ERA5_folder,era5_file)
     year = int(era5_file[-7:-3])
@@ -63,28 +63,24 @@ for era5_file in era5_files[:35]:
     era5_daily['tp'] = era5_daily['tp']*1000.       # convert precipitation from m to mm.
     era5_daily = era5_daily.rename({'tp':'precip'})
 
-    # get the coordiantes of era5 data
-    #era_latmax = era5_daily.coords['latitude'].values.max()
-    #era_lonmax = era5_daily.coords['longitude'].values.max()
-    #era_latmin = era5_daily.coords['latitude'].values.min()
-    #era_lonmin = era5_daily.coords['longitude'].values.min()
-    
     era_lats = era5_daily.latitude.values
     era_lons = era5_daily.longitude.values
     era_dates = era5_daily.date.values
 
-    cmaids = nodes[(nodes.CMAID<(year+1)*100)&(nodes.CMAID>=year*100)].CMAID.unique()
+    #cmaids = nodes[(nodes.CMAID<(year+1)*100)&(nodes.CMAID>=year*100)].CMAID.unique()
     #cmaids = nodes[nodes.CMAID<200101].CMAID.unique()
-    #cmaids = [201805]
+    cmaids = [201523]
 
     for cmaid in cmaids:
         print(cmaid)
+        df_proj = nodes_proj[nodes_proj.CMAID==cmaid].copy()
         df = nodes[nodes.CMAID==cmaid].copy()
+
         # get a rough extent of impact 
-        buffer_zone = df.buffer(zone)
+        buffer_zone = df_proj.buffer(zone)
         polygons = buffer_zone.geometry
-        bounds = gpd.GeoSeries(cascaded_union(polygons))
-        bounds.crs = CRS.from_epsg(proj_epsg)
+        bounds = gpd.GeoSeries(cascaded_union(polygons),crs=proj_epsg)
+        #bounds.crs = CRS.from_epsg(proj_epsg)
         bounds_wgs = bounds.to_crs(epsg=init_epsg)
     
         # prepare coordinates
@@ -93,8 +89,8 @@ for era5_file in era5_files[:35]:
         idlon2 = np.argmin(abs(era_lons-maxlon))
         idlat1 = np.argmin(abs(era_lats-maxlat))
         idlat2 = np.argmin(abs(era_lats-minlat))
-        dummy = era5_daily[dict(date=1, latitude=np.arange(idlat1,idlat2+1), \
-                                longitude=np.arange(idlon1,idlon2+1))]
+        dummy = era5_daily[dict(date=1, latitude=np.arange(idlat1-1,idlat2+1), \
+                                longitude=np.arange(idlon1-1,idlon2+1))]
         coords = dummy.to_dataframe().reset_index().drop(columns=['precip','date'])
         coords['geometry'] = coords.apply(lambda x: Point((float(x.longitude),float(x.latitude))),axis=1)
         coords = gpd.GeoDataFrame(coords,geometry='geometry',crs="epsg:4326")
@@ -112,12 +108,12 @@ for era5_file in era5_files[:35]:
             coords[dd]=False
         
         # Extract precipitation grids
-        grouped = df.groupby('Time')
+        grouped = df_proj.groupby('Time')
         for name, group in grouped:
             buffer1 = group.buffer(zone)
             polygons = buffer1.geometry
-            bounds1 = gpd.GeoSeries(cascaded_union(polygons))
-            bounds1.crs = CRS.from_epsg(proj_epsg)
+            bounds1 = gpd.GeoSeries(cascaded_union(polygons),crs=proj_epsg)
+            #bounds1.crs = CRS.from_epsg(proj_epsg)
             bounds1_wgs = bounds1.to_crs(epsg=init_epsg)
     
             idx1 = coords.geometry.apply(lambda x: x.within(bounds1_wgs[0]))
@@ -140,8 +136,8 @@ for era5_file in era5_files[:35]:
         for (ii,jj) in zip(days,doys):
 
             #print(ii,jj)
-            precip = era5_daily[dict(date=jj, latitude=np.arange(idlat1,idlat2+1), \
-                                     longitude=np.arange(idlon1,idlon2+1))]
+            precip = era5_daily[dict(date=jj, latitude=np.arange(idlat1-1,idlat2+1), \
+                                     longitude=np.arange(idlon1-1,idlon2+1))]
             dummy1 = precip.to_dataframe().reset_index().drop(columns=['date'])
             dummy1['Time'] = ii
             dummy2 = dummy1[coords[ii]]
@@ -150,8 +146,8 @@ for era5_file in era5_files[:35]:
                 first_call = 0
             else: 
                 precip_daily = precip_daily.append(dummy2)   
-            
-            precip_total = precip_daily.groupby(['latitude','longitude'])['precip'].sum().to_frame().reset_index()
+        precip_daily = precip_daily.fillna(0.)    
+        precip_total = precip_daily.groupby(['latitude','longitude'])['precip'].sum().to_frame().reset_index()
         #%%
         daily_file = str(cmaid) + '_' + str(df.Year.iloc[0]) + '_' + \
             df.Name.iloc[0] + '_daily_precip_ERA5_' + \
@@ -161,11 +157,11 @@ for era5_file in era5_files[:35]:
             df.Name.iloc[0] + '_total_precip_ERA5_' + \
             str(int(zone/1000))+'km_' + str(int(pre_days))+str(int(post_days))+'.pkl'
     
-        precip_daily.to_pickle(os.path.join(Output_folder, 'ERA5', daily_file))
-        precip_total.to_pickle(os.path.join(Output_folder, 'ERA5', total_file))
+        precip_daily.to_pickle(os.path.join(Output_folder, 'ERA5_1500km_12', daily_file))
+        precip_total.to_pickle(os.path.join(Output_folder, 'ERA5_1500km_12', total_file))
     
         #%% Plotting
-        df_wgs = df.to_crs(epsg=init_epsg)
+        #df_wgs = df.to_crs(epsg=init_epsg)
         # convert dataframe to geodataframe for plotting
     
         precip_total = gpd.GeoDataFrame(precip_total, \
@@ -174,15 +170,18 @@ for era5_file in era5_files[:35]:
         fig,ax = plt.subplots(1,1)
         divider = make_axes_locatable(ax)
         cax = divider.append_axes("right", size="5%", pad=0.01)
-        cmax = precip_total.precip.mean() + precip_total.precip.std()
+        cmax = precip_total.precip.mean() + 3*precip_total.precip.std()
+        
+        
         precip_total.plot(column='precip', ax=ax, \
     #                    cmap='Blues', scheme='quantiles', \
                           cmap='Blues', vmin=0,vmax=cmax, cax=cax, \
     #                  cmap='Blues', scheme='naturalbreaks', cax=cax, \
                           legend=True, legend_kwds={'label': "Total Precip. (mm)"})
         cn_shape.boundary.plot(color='black',ax=ax)
-        df_wgs.plot(ax=ax,markersize=2,color='red')
-        ax.title.set_text(df.Name.iloc[0] + ' ' + days[0] + '-' + days[-1])
+        df.plot(ax=ax,markersize=2,color='red')
+        plt.text(0.3, 0.9, 'Max. total precip = %d mm'%precip_total.precip.max(), horizontalalignment='center',verticalalignment='center', transform=ax.transAxes)
+        ax.title.set_text('ERA5 ' + df.Name.iloc[0] + ' ' + days[0] + '-' + days[-1])
         ax.set_xlim([70,140])
         ax.set_ylim([15,55])    
         ax.set_xlabel('Longitude')
@@ -191,7 +190,7 @@ for era5_file in era5_files[:35]:
             df.Name.iloc[0] + '_total_precip_ERA5_' + str(int(zone/1000)) + \
             'km_' + str(int(pre_days)) + str(int(post_days)) + '.png'
     
-        fig.savefig(os.path.join(Output_folder,'ERA5', figname))
+        fig.savefig(os.path.join(Output_folder,'ERA5_1500km_12', figname))
         plt.close(fig)
 
 
