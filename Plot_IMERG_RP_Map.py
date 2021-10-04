7659fb1a0ece9ca93969bba9a23463c25c2e4acd
lzhou <luxi.zhou@outlook.com>
2021-10-04 23:00:10 +0800

    minor edit to RP plot script

--- Plot_IMERG_RP_Map.py
+++ Plot_IMERG_RP_Map.py
@@ -7,21 +7,15 @@ Created on Sun Oct  3 15:17:51 2021
 """
 
 import numpy as np
-#import glob
 import os
 import pandas as pd
-#import pickle
 import geopandas as gpd
-#from datetime import timedelta
-#from shapely.geometry import Point
 import matplotlib.pyplot as plt
-#import geopandas as gpd
-#from pyproj import CRS
-#from shapely.ops import cascaded_union
-#import seaborn as sns
 import shapely
 from mpl_toolkits.axes_grid1 import make_axes_locatable
+import matplotlib
 shapely.speedups.enabled
+
 #%%
 wrk_dir = '/home/lzhou/Precipitation/Precipitation_Scripts'
 os.chdir(wrk_dir)
@@ -31,7 +25,7 @@ case_name = 'IMERG_1500km_12'
 factor = 'total'
 output_dir = '/home/lzhou/Precipitation/Output'
 data_dir = os.path.join(output_dir,case_name)
-
+figure_dir = '/home/lzhou/Precipitation/Output/Figures'
 #%% combine total precipitation data from each event
 
 # dummy = os.listdir(data_dir)
@@ -61,7 +55,7 @@ infile = os.path.join(data_dir,filename)
 df = pd.read_pickle(infile)
 df.sort_values(by=['lat','lon'],ignore_index=True,inplace=True)
 #%% sort event total preciptation for each grid
-data0 = df.iloc[:,23:].to_numpy()
+data0 = df.iloc[:,23:].to_numpy()   # This is counting from 2001-2020, IMERG Data
 data1 = np.sort(-data0,axis=1) 
 data2 = -data1
 sorted_df = pd.DataFrame(data2)
@@ -81,26 +75,34 @@ china_points = sorted_df.loc[idx].copy()
 rps = np.array([20,10,5,4,2,1])
 ranks = 20/rps
 #%%
+
+font = {'family' : 'normal',
+        'size'   : 16}
+matplotlib.rc('font', **font)
+#%%
 for ii in np.arange(0,len(ranks)):
     print(ii)
-    fig,ax = plt.subplots(1,1,figsize=(12,8))
+    fig,ax = plt.subplots(1,1,figsize=(8,6))
 
     divider = make_axes_locatable(ax)
-    cax = divider.append_axes("right", size="5%", pad=0.01)
+    cax = divider.append_axes("right", size="3%", pad=-0.3)
     cmax = china_points.iloc[:,int(ranks[ii])].mean()+china_points.iloc[:,int(ranks[ii])].std()*3.
     china_points.plot(column=int(ranks[ii]), ax=ax, 
                       cmap='Spectral_r', \
                       vmin=0, vmax=cmax, \
                       cax=cax,legend=True, legend_kwds={'label': "Event Total Precip. (mm)"})
     cn_shape.boundary.plot(color='black',ax=ax)
-#df.plot(ax=ax,markersize=2,color='red')
+
     plt.text(0.3, 0.9, 'Max. total precip = %d mm'%china_points.iloc[:,int(ranks[ii])].max(), horizontalalignment='center',verticalalignment='center', transform=ax.transAxes)
 
-    ax.title.set_text(case_name + '_' + str(rps[ii]) + 'Y')
+    #ax.title.set_text(case_name + '_ReturnPeriod' + str(rps[ii]) + 'Y')
+    title_str = case_name + '_' + str(rps[ii]) + 'Y'
+    ax.set_title(title_str,fontweight='bold')
     ax.set_xlim([70,140])
     ax.set_ylim([15,55])    
     ax.set_xlabel('Longitude')
     ax.set_ylabel('Latitude')                              
     figname = case_name+'_RP'+str(rps[ii]) + 'Y_Event_Total_Precip' + '.png'
-    fig.savefig(os.path.join('/home/lzhou/Precipitation/Output','IMERG_1500km_12', figname))
+    fig.tight_layout()
+    fig.savefig(os.path.join(figure_dir, figname))
     plt.close(fig)
